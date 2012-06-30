cd0cb0fbf4d50e6131f0e5aac01825f2ff383577
tsug <bertrand.quenin@gmail.com>
2012-07-01 01:03:26 +0200

    upgrading

--- generic/HelloWorldScene.h
+++ generic/HelloWorldScene.h
@@ -3,20 +3,19 @@
 
 #include "cocos2d.h"
 
-class HelloWorld : public cocos2d::CCLayer
-{
+class HelloWorld : public cocos2d::CCLayer {
 public:
-    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-    virtual bool init();  
+  // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+  virtual bool init();
 
-    // there's no 'id' in cpp, so we recommand to return the exactly class pointer
-    static cocos2d::CCScene* scene();
-    
-    // a selector callback
-    void menuCloseCallback(CCObject* pSender);
+  // there's no 'id' in cpp, so we recommand to return the exactly class pointer
+  static cocos2d::CCScene *scene();
 
-    // implement the "static node()" method manually
-    LAYER_CREATE_FUNC(HelloWorld);
+  // a selector callback
+  void menuCloseCallback(CCObject *pSender);
+
+  // implement the "static node()" method manually
+  LAYER_CREATE_FUNC(HelloWorld);
 };
 
 #endif // __HELLOWORLD_SCENE_H__
