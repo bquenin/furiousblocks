a367511fab241702c81f1b2711f1f9da584b454f
bquenin <bertrand.quenin@gmail.com>
2012-11-25 22:45:04 +0100

    removing ccb stuff

---
+++ generic/scenes/LogoScene.cpp
@@ -0,0 +1,8 @@
+//
+// Created by bquenin on 11/25/12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#include "LogoScene.h"
---
+++ generic/scenes/LogoScene.h
@@ -0,0 +1,19 @@
+//
+// Created by bquenin on 11/25/12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+
+#ifndef __LogoScene_H_
+#define __LogoScene_H_
+
+#include <iostream>
+
+
+class LogoScene {
+
+};
+
+#endif //__LogoScene_H_
