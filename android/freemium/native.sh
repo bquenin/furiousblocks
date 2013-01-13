47463c36789cfe069b33f8cf98bd4aa60536935a
bquenin <bertrand.quenin@gmail.com>
2013-01-13 01:11:31 +0100

    Updating to poco 1.5.1 release and fix build & link processes for iOS & Android

--- android/freemium/native.sh
+++ android/freemium/native.sh
@@ -9,7 +9,6 @@ echo "APP_ROOT = $APP_ROOT"
 echo "APP_ANDROID_ROOT = $APP_ANDROID_ROOT"
 
 echo "Using prebuilt externals"
-"$NDK_ROOT"/ndk-build -C "$APP_ANDROID_ROOT" $* \
-    "NDK_MODULE_PATH=${COCOS2DX_ROOT}:${COCOS2DX_ROOT}/cocos2dx/platform/third_party/android/prebuilt"
+"$NDK_ROOT"/ndk-build -C "$APP_ANDROID_ROOT" $* "NDK_MODULE_PATH=${COCOS2DX_ROOT}:${COCOS2DX_ROOT}/cocos2dx/platform/third_party/android/prebuilt" V=1 -j16
 
 cp obj/local/armeabi/libfuriousblocks.so libs/armeabi/libfuriousblocks.so
