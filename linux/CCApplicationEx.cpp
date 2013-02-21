68a5dd1c9922f8b17416f041728b5c6900df71ac
tsug <bertrand.quenin@gmail.com>
2013-02-21 17:24:06 +0100

    Adding a linux version

---
+++ linux/CCApplicationEx.cpp
@@ -0,0 +1,29 @@
+//
+// Created by bquenin on 1/29/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#include "CCApplicationEx.h"
+#include "Social.h"
+#include "AppDelegate.h"
+
+using namespace cocos2d;
+
+CCApplicationEx::CCApplicationEx()
+: CCApplication() {
+}
+
+void CCApplicationEx::openURL(const std::string& url) {
+}
+
+void CCApplicationEx::facebookLogin() {
+}
+
+void CCApplicationEx::facebookLogout() {
+}
+
+void CCApplicationEx::quit() {
+    exit(0);
+}
